eb0f156d578d757607beaf01dc7b1e80885f9c39
Giorgos Patsiaouras <giorgospatsiaouras@gmail.com>
2019-02-12 04:14:11 +0100

    Working kind of but random stuff are happening

--- src/particle.py
+++ src/particle.py
@@ -1,14 +1,21 @@
 import numpy as np
+import math
 
 # Constants
-w = 0.5
-c1 = 0.5
-c2 = 0.5
+a = 0.4
+b = 2.0
+c = 2.0
+
 
 class Particle:
-    def __init__(self, dimensions, lower_bound, upper_bound):
+    def __init__(self, lower_bound, upper_bound):
+        # Assign local variables
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+
         # Initialize the particle's position with a uniformly distributed random vector
-        self.position = np.random.uniform(lower_bound, upper_bound, dimensions)  # assign random possition
+        self.position = np.random.uniform(lower_bound, upper_bound, 2)  # assign random possition
+
         # Initialize the particle's best known position to its initial position
         self.best_position = self.position
         self.velocity = np.array([0, 0])
@@ -16,31 +23,33 @@ class Particle:
     def rosenbrock(self, x, y):
         return (1 - x) ** 2 + 100 * ((y - x ** 2)) ** 2
 
+    def rastrigin(self, x, y):
+        return 0 - (10 * 2 + (x ** 2 - (10 * np.cos(2 * np.pi * x))) + (y ** 2 - (10 * np.cos(2 * np.pi * y))))
+
     def evaluation_of_current_position(self):
+        # return self.rosenbrock(self.position[0], self.position[1])
         return self.rosenbrock(self.position[0], self.position[1])
 
     def evaluation_of_best_position(self):
-        return self.rosenbrock(self.best_position[0], self.best_position[1])
+        # return self.rosenbrock(self.best_position[0], self.best_position[1])
+        return self.rosenbrock(self.position[0], self.position[1])
 
     def move_to_new_position(self):
-        global w
-        global c1
-        global c2
-        # Get random numbers
-        rx = np.random.uniform(0, 1)
-        ry = np.random.uniform(0, 1)
+        new_position = self.position + self.velocity
+        if self.lower_bound <= new_position[0] <= self.upper_bound \
+                and self.lower_bound <= new_position[1] <= self.upper_bound:
+            self.position = new_position
+
+    def update_velocity(self, global_best_position):
+        global a
+        global b
+        global c
 
         # calculate new velocity
-        self.velocity = w * self.velocity + c1 * rx
-        # TODO Do magic and move it to another position
-        #       for each dimension d = 1, ..., n do
-        #          Pick random numbers: rp, rg ~ U(0,1)
-        #          Update the particle's velocity: vi,d ← ω vi,d + φp rp (pi,d-xi,d) + φg rg (gd-xi,d)
-        #       Update the particle's position: xi ← xi + vi
-        pass
-
-    def update_velocity(self):
-        pass
+        self.velocity = a * self.velocity + (b * np.random.uniform(0, 1)) * (
+                    self.best_position - self.position) + (c * np.random.uniform(0, 1)) * (
+                                       global_best_position - self.position)
 
     def initialize_velocity(self):
-        pass
+        abs_diff = np.fabs(self.lower_bound - self.upper_bound)
+        self.velocity = np.random.uniform(-abs_diff, abs_diff, 2)
--- src/pso.py
+++ src/pso.py
@@ -1,27 +1,20 @@
-import numpy
+import numpy as np
 from particle import Particle
+import matplotlib.pyplot as plt
 
 
 class PSO:
-    def __init__(self, number_of_particles, dimensions, iterations):
+    def __init__(self, number_of_particles, iterations):
 
         # Initialize coefficients and constants
         self.iterations = iterations
-        self.dimensions = dimensions
         self.evaluation_of_global_position = float('inf')
-        self.lower_bound = 0
+        self.global_position = np.array([np.random.random() * 50, np.random.random() * 50])
+        self.lower_bound = -4
         self.upper_bound = 4
 
-        # TODO this is not yet implemented
-        #  for each particle i = 1, ..., S do
-        #    Initialize the particle's position with a uniformly distributed random vector: xi ~ U(blo, bup)
-        #    Initialize the particle's best known position to its initial position: pi ← xi
-        #    if f(pi) < f(g) then
-        #        update the swarm's best known  position: g ← pi
-        #    Initialize the particle's velocity: vi ~ U(-|bup-blo|, |bup-blo|)
-
         # Initialize particles
-        self.particles = [Particle(dimensions, self.lower_bound, self.upper_bound) for n in range(number_of_particles)]
+        self.particles = [Particle(self.lower_bound, self.upper_bound) for n in range(number_of_particles)]
         for particle in self.particles:
             if particle.evaluation_of_best_position() < self.evaluation_of_global_position:
                 self.global_position = particle.best_position
@@ -29,9 +22,29 @@ class PSO:
             particle.initialize_velocity()
 
     def run(self):
-        for iteration in self.iterations:
+
+        plt.ion()
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        X = []
+        Y = []
+        Z = []
+        sc = ax.scatter(np.array(X), np.array(Y))
+        ax.set_xlim([self.lower_bound, self.upper_bound])
+        ax.set_ylim([self.lower_bound, self.upper_bound])
+        fig.show()
+
+        for iteration in range(self.iterations):
+            X = []
+            Y = []
+            Z = []
             for particle in self.particles:
-                # more stuff
+                # For the plot
+                X.append(particle.position[0])
+                Y.append(particle.position[1])
+                Z.append(particle.evaluation_of_current_position())
+
+                particle.update_velocity(self.global_position)
                 particle.move_to_new_position()
                 if particle.evaluation_of_current_position() < particle.evaluation_of_best_position():
                     particle.best_position = particle.position
@@ -39,26 +52,24 @@ class PSO:
                         self.evaluation_of_global_position = particle.evaluation_of_best_position()
                         self.global_position = particle.best_position
 
+            ax.cla()
+            sc = ax.scatter(np.array(X), np.array(Y))
+            ax.set_xlim([self.lower_bound, self.upper_bound])
+            ax.set_ylim([self.lower_bound, self.upper_bound])
+            fig.canvas.draw()
+            plt.pause(0.01)
+
+            if not iteration % 10:
+                print("Iteration " + str(iteration))
+        print("Best position is on [%f, %f] with value %f" % (self.global_position[0], self.global_position[1], self.evaluation_of_global_position))
+
 
 if __name__ == '__main__':
 
     # Define variables of Particle Swarm Optimization
-    number_of_particles = 200
-    dimensions = 2
+    number_of_particles = 10
     iterations = 1000
 
     # Execute PSO
-    pso = PSO(number_of_particles, dimensions, iterations)
+    pso = PSO(number_of_particles, iterations)
     pso.run()
-
-
-# while a termination criterion is not met do:
-#    for each particle i = 1, ..., S do
-#       for each dimension d = 1, ..., n do
-#          Pick random numbers: rp, rg ~ U(0,1)
-#          Update the particle's velocity: vi,d ← ω vi,d + φp rp (pi,d-xi,d) + φg rg (gd-xi,d)
-#       Update the particle's position: xi ← xi + vi
-#       if f(xi) < f(pi) then
-#          Update the particle's best known position: pi ← xi
-#          if f(pi) < f(g) then
-#             Update the swarm's best known position: g ← pi
